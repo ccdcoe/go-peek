73975391a8c2d69d84d547ad3b9ae790b8fafff7
Markus Kont <alias013@gmail.com>
2019-01-02 21:54:30 +0000

    Contextualize functions and reduce object overhead.

--- internal/ingest/kafka/kafka.go
+++ internal/ingest/kafka/kafka.go
@@ -10,9 +10,8 @@ import (
 )
 
 type KafkaIngest struct {
-	output chan message.Message
-	ctx    context.Context
-	cancel context.CancelFunc
+	output        chan message.Message
+	consumeCansel context.CancelFunc
 
 	*cluster.Consumer
 }
@@ -20,6 +19,7 @@ type KafkaIngest struct {
 func NewKafkaIngest(config *KafkaConfig) (*KafkaIngest, error) {
 	var (
 		err error
+		ctx context.Context
 		k   = &KafkaIngest{
 			output: make(chan message.Message, 0),
 		}
@@ -36,8 +36,8 @@ func NewKafkaIngest(config *KafkaConfig) (*KafkaIngest, error) {
 	); err != nil {
 		return nil, err
 	}
-	k.ctx, k.cancel = context.WithCancel(context.Background())
-	go func() {
+	ctx, k.consumeCansel = context.WithCancel(context.Background())
+	go func(ctx context.Context) {
 	loop:
 		for {
 			select {
@@ -50,11 +50,11 @@ func NewKafkaIngest(config *KafkaConfig) (*KafkaIngest, error) {
 					Offset: msg.Offset,
 					Source: msg.Topic,
 				}
-			case <-k.ctx.Done():
+			case <-ctx.Done():
 				k.Consumer.Close()
 			}
 		}
-	}()
+	}(ctx)
 
 	// *TODO* Move to separate notification handler
 	go func() {
@@ -71,6 +71,6 @@ func (k KafkaIngest) Messages() <-chan message.Message {
 }
 
 func (k KafkaIngest) Halt() error {
-	k.cancel()
-	return k.ctx.Err()
+	k.consumeCansel()
+	return nil
 }
