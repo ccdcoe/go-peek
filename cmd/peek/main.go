3259d1e1892b73ab6ce574e944c145ca928efd26
Markus Kont <alias013@gmail.com>
2019-01-03 12:43:29 +0200

    Restructure main for multiple subcommands.

--- cmd/peek/main.go
+++ cmd/peek/main.go
@@ -12,10 +12,6 @@ import (
 	"github.com/ccdcoe/go-peek/internal/ingest/kafka"
 )
 
-const (
-	onliemode = "online"
-)
-
 var (
 	mainFlags = flag.NewFlagSet("main", flag.ExitOnError)
 	confPath  = mainFlags.String("config", path.Join(
@@ -24,6 +20,25 @@ var (
 	exampleConf = mainFlags.Bool("example-config", false,
 		`Print default TOML config and exit`)
 )
+
+var usageStr = `
+Usage: peek [options] [command] [args]
+
+Commands:
+
+	online					Process online kafka streams, default option
+	consume					Read messages from configured kafka topics and print messages to stdout
+	replay					Replay log files to simulate kafka stream
+	help					Print commands and exit
+
+Options:
+`
+
+func usage() {
+	fmt.Fprintln(os.Stderr, usageStr)
+	mainFlags.PrintDefaults()
+	os.Exit(1)
+}
 
 func main() {
 	mainFlags.Parse(os.Args[1:])
@@ -41,34 +56,37 @@ func main() {
 		appConfg = config.NewDefaultConfig()
 	)
 
-	fmt.Fprintf(os.Stdout, "Loading main config\n")
-	if _, err = toml.DecodeFile(*confPath, &appConfg); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-		os.Exit(1)
-	}
-
-	if appConfg.DefaultStreams() {
-		fmt.Fprintf(os.Stdout, "No streams configured, using default\n")
-	}
-
 	args = mainFlags.Args()
 	if len(args) == 0 {
 		fmt.Fprintf(os.Stdout, "No command set, assiming online\n")
-		command = onliemode
+		command = "online"
 	} else {
 		command = args[0]
 		args = args[1:]
 	}
 
 	switch command {
-	case onliemode:
+	case "online":
+		commandF = doOnlineProcess
+	case "consume":
 		commandF = doOnlineConsume
 	case "replay":
 		commandF = doReplay
+	case "help":
+		usage()
 	default:
-		// *TODO* print list of commands
-		fmt.Fprintf(os.Stderr, "Unknown command, exiting\n")
+		fmt.Fprintf(os.Stderr, "Unknown command, %s\n", command)
+		usage()
+	}
+
+	fmt.Fprintf(os.Stdout, "Loading main config\n")
+	if _, err = toml.DecodeFile(*confPath, &appConfg); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 		os.Exit(2)
+	}
+
+	if appConfg.DefaultStreams() {
+		fmt.Fprintf(os.Stdout, "No streams configured, using default\n")
 	}
 
 	fmt.Fprintf(os.Stdout, "Running main command\n")
@@ -77,9 +95,34 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 		os.Exit(2)
 	}
+	fmt.Fprintf(os.Stdout, "Done\n")
 }
 
+func doHelp(args []string, appConfg *config.Config) error {
+	usage()
+	return nil
+}
 func doOnlineConsume(args []string, appConfg *config.Config) error {
+	var (
+		consumer ingest.Ingester
+		err      error
+	)
+
+	fmt.Fprintf(os.Stdout, "Starting consumer\n")
+	if consumer, err = kafka.NewKafkaIngest(
+		appConfg.KafkaConfig(),
+	); err != nil {
+		return err
+	}
+
+	// *TODO* temp code during devel, remove
+	fmt.Fprintf(os.Stdout, "Processing messages\n")
+	for msg := range consumer.Messages() {
+		fmt.Println(string(msg.Data))
+	}
+	return nil
+}
+func doOnlineProcess(args []string, appConfg *config.Config) error {
 	var (
 		consumer ingest.Ingester
 		err      error
