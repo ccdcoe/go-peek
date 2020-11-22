4aae46ca907e410f3bb021561183853acaee256b
Markus Kont <markus@stamus-networks.com>
2020-11-22 16:59:05 +0200

    scripts: limit rate for development

--- scripts/logs-to-socket.py
+++ scripts/logs-to-socket.py
@@ -4,11 +4,20 @@ import argparse
 import glob
 import gzip
 import socket
+import time
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--glob", dest="glob", default=None)
-    parser.add_argument("--socket", dest="sock", default=None)
+    parser.add_argument("--glob",
+                        dest="glob",
+                        default=None)
+    parser.add_argument("--socket",
+                        dest="sock",
+                        default=None)
+    parser.add_argument("--limit-rate",
+                        dest="limit_rate",
+                        default=False,
+                        action="store_true")
     args = parser.parse_args()
 
     files = glob.glob(args.glob)
@@ -22,4 +31,6 @@ if __name__ == "__main__":
         print(file)
         with gzip.open(file, "r") as handle:
             for line in handle:
+                if args.limit_rate:
+                    time.sleep(1)
                 client.send(line)
