0dde17948280ee0fdcf26bc83c66ba2c78234223
Markus Kont <markus@stamus-networks.com>
2020-11-25 23:45:45 +0200

    ingest: better kafka logging

--- pkg/ingest/kafka/kafka.go
+++ pkg/ingest/kafka/kafka.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"time"
 
 	"go-peek/pkg/models/consumer"
 	"go-peek/pkg/utils"
 
 	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
 )
 
 var Version = "2.6.0"
@@ -34,7 +36,8 @@ func NewConsumer(c *Config) (*Consumer, error) {
 		wg:     &sync.WaitGroup{},
 		config: sarama.NewConfig(),
 		handle: &handle{
-			messages: make(chan *consumer.Message, 0),
+			messages:    make(chan *consumer.Message, 0),
+			logInterval: 30 * time.Second,
 		},
 		errs: utils.NewErrChan(100, fmt.Sprintf(
 			"kafka consumer for brokers %+v topics %+v",
@@ -56,6 +59,7 @@ func NewConsumer(c *Config) (*Consumer, error) {
 		return nil, err
 	}
 	obj.config.Version = version
+	obj.config.Consumer.Return.Errors = true
 
 	group, err := sarama.NewConsumerGroup(c.Brokers, c.ConsumerGroup, obj.config)
 	if err != nil {
@@ -70,6 +74,10 @@ func NewConsumer(c *Config) (*Consumer, error) {
 			select {
 			case <-obj.ctx.Done():
 				break loop
+			case err := <-obj.group.Errors():
+				if err != nil {
+					obj.errs.Send(err)
+				}
 			default:
 				if err := obj.group.Consume(obj.ctx, c.Topics, obj.handle); err != nil {
 					obj.errs.Send(err)
@@ -93,11 +101,14 @@ func (c Consumer) Errors() <-chan error {
 
 // handle represents a Sarama consumer group consumer
 type handle struct {
-	messages chan *consumer.Message
+	messages    chan *consumer.Message
+	logInterval time.Duration
+	name        string
 }
 
 // Setup is run at the beginning of a new session, before ConsumeClaim
-func (c *handle) Setup(sarama.ConsumerGroupSession) error {
+func (c *handle) Setup(session sarama.ConsumerGroupSession) error {
+	logrus.Tracef("Sarama consumer claimed: %+v", session.Claims())
 	return nil
 }
 
@@ -106,26 +117,42 @@ func (c *handle) Cleanup(sarama.ConsumerGroupSession) error {
 	return nil
 }
 
+func (c handle) traceLog(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) {
+	logrus.Tracef(
+		"name: %s session_member_id: %s high_watermark_offset: %d topic: %s",
+		c.name,
+		session.MemberID(),
+		claim.HighWaterMarkOffset(),
+		claim.Topic(),
+	)
+}
+
 // ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
 func (c *handle) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 
-	// NOTE:
-	// Do not move the code below to a goroutine.
-	// The `ConsumeClaim` itself is called within a goroutine, see:
-	// https://github.com/Shopify/sarama/blob/master/consumer_group.go#L27-L29
-	for msg := range claim.Messages() {
-		c.messages <- &consumer.Message{
-			Partition: int64(msg.Partition),
-			Data:      msg.Value,
-			Offset:    msg.Offset,
-			Source:    msg.Topic,
-			Time:      msg.Timestamp,
-			Key:       string(msg.Key),
-			Type:      consumer.Kafka,
+	logTick := time.NewTicker(c.logInterval)
+	first := time.NewTimer(10 * time.Second)
+loop:
+	for {
+		select {
+		case <-first.C:
+			c.traceLog(session, claim)
+		case <-logTick.C:
+			c.traceLog(session, claim)
+		case msg, ok := <-claim.Messages():
+			if !ok {
+				break loop
+			}
+			c.messages <- &consumer.Message{
+				Partition: int64(msg.Partition),
+				Data:      msg.Value,
+				Offset:    msg.Offset,
+				Source:    msg.Topic,
+				Time:      msg.Timestamp,
+				Key:       string(msg.Key),
+				Type:      consumer.Kafka,
+			}
 		}
-		// // TODO - autocommit?
-		// session.MarkMessage(msg, "")
 	}
-
 	return nil
 }
