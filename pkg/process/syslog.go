021648cd1af7b50c88c8cff0644a2cdf47a3680c
Markus Kont <markus@stamus-networks.com>
2021-11-03 08:49:34 +0200

    preprocess: set up syslog server

---
+++ pkg/process/syslog.go
@@ -0,0 +1,75 @@
+package process
+
+import (
+	"context"
+	"errors"
+	"net"
+	"sync"
+	"time"
+)
+
+type SyslogServer struct {
+	Listener  *net.UDPConn
+	Collector *Collector
+	Errors    chan error
+}
+
+func (s SyslogServer) Run(wg *sync.WaitGroup, ctx context.Context) error {
+	if s.Listener == nil {
+		return errors.New("missing UDP handler")
+	}
+	if wg == nil {
+		return errors.New("missing waitgroup")
+	}
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		defer s.Listener.Close()
+
+		buf := make([]byte, 1024*64)
+	loop:
+		for {
+			select {
+			case <-ctx.Done():
+				break loop
+			default:
+			}
+			s.Listener.SetDeadline(time.Now().Add(1e9))
+			n, _, err := s.Listener.ReadFromUDP(buf)
+			if err != nil {
+				if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					continue loop
+				}
+				select {
+				case s.Errors <- err:
+				default:
+				}
+			}
+			s.Collector.Collect(buf[:n])
+		}
+	}(ctx)
+	return nil
+}
+
+func NewSyslogServer(fn CollectBulkFullFn) (*SyslogServer, error) {
+	if fn == nil {
+		return nil, errors.New("missing bulk handler func")
+	}
+	server := &SyslogServer{}
+	addr, err := net.ResolveUDPAddr("udp", "0.0.0.0:514")
+	if err != nil {
+		return nil, err
+	}
+	listener, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	server.Listener = listener
+	server.Collector = &Collector{
+		Size:        256 * 1024,
+		Ticker:      time.NewTicker(1 * time.Second),
+		HandlerFunc: fn,
+	}
+	server.Errors = make(chan error, 10)
+	return server, nil
+}
