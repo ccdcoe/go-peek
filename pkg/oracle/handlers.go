d619dfb709278bd67ff0ac379a012548bf3e4441
Markus Kont <markus@stamus-networks.com>
2021-11-10 13:38:59 +0200

    oracle: split routes and handlers

---
+++ pkg/oracle/handlers.go
@@ -0,0 +1,50 @@
+package oracle
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *Server) handleIndex() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		get := map[string]string{
+			"/":             "API routes listing",
+			"/assets":       "asset listing",
+			"/mitremeerkat": "suricata SID to MITRE mappings",
+		}
+		d, err := json.Marshal(get)
+		if err != nil {
+			rw.WriteHeader(500)
+			return
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(200)
+		rw.Write(d)
+	}
+}
+
+func (s *Server) handleAssets() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		data, err := s.Assets.JSON()
+		if err != nil {
+			rw.WriteHeader(500)
+			return
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(200)
+		rw.Write(data)
+	}
+}
+
+func (s *Server) handleMitreMeerkat() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		data, err := s.SidMap.JSON()
+		if err != nil {
+			rw.WriteHeader(500)
+			return
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(200)
+		rw.Write(data)
+	}
+}
--- pkg/oracle/routes.go
+++ pkg/oracle/routes.go
@@ -1,9 +1,6 @@
 package oracle
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
@@ -14,48 +11,4 @@ func (s *Server) Routes() {
 	s.Router.HandleFunc("/", s.handleIndex())
 	s.Router.HandleFunc("/assets", s.handleAssets())
 	s.Router.HandleFunc("/mitremeerkat", s.handleMitreMeerkat())
-}
-
-func (s *Server) handleIndex() http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		get := map[string]string{
-			"/":             "API routes listing",
-			"/assets":       "asset listing",
-			"/mitremeerkat": "suricata SID to MITRE mappings",
-		}
-		d, err := json.Marshal(get)
-		if err != nil {
-			rw.WriteHeader(500)
-			return
-		}
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(200)
-		rw.Write(d)
-	}
-}
-
-func (s *Server) handleAssets() http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		data, err := s.Assets.JSON()
-		if err != nil {
-			rw.WriteHeader(500)
-			return
-		}
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(200)
-		rw.Write(data)
-	}
-}
-
-func (s *Server) handleMitreMeerkat() http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		data, err := s.SidMap.JSON()
-		if err != nil {
-			rw.WriteHeader(500)
-			return
-		}
-		rw.Header().Set("Content-Type", "application/json")
-		rw.WriteHeader(200)
-		rw.Write(data)
-	}
 }
