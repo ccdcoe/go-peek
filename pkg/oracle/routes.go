2d7a41bf6681dafbb7271ba9243987cbf3a7e41b
Markus Kont <markus@stamus-networks.com>
2021-11-25 05:54:49 +0200

    oracle: routes fmt max line len reduce

--- pkg/oracle/routes.go
+++ pkg/oracle/routes.go
@@ -8,11 +8,32 @@ import (
 func (s *Server) Routes() {
 	s.Router = mux.NewRouter()
 
-	s.Router.HandleFunc("/", s.handleIndex())
-	s.Router.HandleFunc("/assets", s.handleAssets()).Methods("GET")
-	s.Router.HandleFunc("/mitremeerkat/mappings", s.handleMitreMeerkat()).Methods("GET")
-	s.Router.HandleFunc("/mitremeerkat/missing", s.handleMitreMeerkatMissing()).Methods("GET")
-	s.Router.HandleFunc("/ioc", s.handleIoCServe()).Methods("GET")
-	s.Router.HandleFunc("/ioc/add", s.handleIoCAdd()).Methods("POST")
-	s.Router.HandleFunc("/ioc/meerkat", s.handleIoCAdd()).Methods("GET")
+	s.Router.HandleFunc(
+		"/",
+		s.handleIndex(),
+	)
+	s.Router.HandleFunc(
+		"/assets",
+		s.handleAssets(),
+	).Methods("GET")
+	s.Router.HandleFunc(
+		"/mitremeerkat/mappings",
+		s.handleMitreMeerkat(),
+	).Methods("GET")
+	s.Router.HandleFunc(
+		"/mitremeerkat/missing",
+		s.handleMitreMeerkatMissing(),
+	).Methods("GET")
+	s.Router.HandleFunc(
+		"/ioc",
+		s.handleIoCServe(),
+	).Methods("GET")
+	s.Router.HandleFunc(
+		"/ioc/add",
+		s.handleIoCAdd(),
+	).Methods("POST")
+	s.Router.HandleFunc(
+		"/ioc/meerkat",
+		s.handleIoCAdd(),
+	).Methods("GET")
 }
