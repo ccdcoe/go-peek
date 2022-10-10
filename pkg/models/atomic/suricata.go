dd4effdbbfa513bbb1a486d498506b207bd62976
Markus Kont <markus@stamus-networks.com>
2022-10-10 18:47:52 +0300

    models: implement stubs for dynamic EVE

--- pkg/models/atomic/suricata.go
+++ pkg/models/atomic/suricata.go
@@ -1,12 +1,42 @@
 package atomic
 
 import (
-	"time"
-
 	"go-peek/pkg/models/fields"
+	"time"
 )
 
 type DynamicSuricataEve map[string]interface{}
+
+func (d DynamicSuricataEve) Time() time.Time {
+	key := "@timestamp"
+	if val, ok := d[key]; ok {
+		switch v := val.(type) {
+		case time.Time:
+			return v
+		case string:
+			if ts, err := time.Parse(time.RFC3339, v); err == nil {
+				return ts
+			}
+		}
+	}
+	return time.Time{}
+}
+
+func (d DynamicSuricataEve) Source() string {
+	val, ok := d["event_type"].(string)
+	if ok {
+		return val
+	}
+	return ""
+}
+
+func (d DynamicSuricataEve) Sender() string {
+	val, ok := d["host"].(string)
+	if ok {
+		return val
+	}
+	return ""
+}
 
 type StaticSuricataEve struct {
 	EveBase
